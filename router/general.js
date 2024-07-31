7d41a9e955430c3ee9d192081db14a0000269718
anu6420 <axa3537@tamu.edu>
2024-07-31 17:22:53 -0500

    Update general.js

--- router/general.js
+++ router/general.js
@@ -29,33 +29,33 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   res.send(JSON.stringify(books));
 
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   const isbn = req.params.isbn;
   res.send(books[isbn]);
 
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   const author = req.params.author;
-  const valuesInList = Object.values(books);
+  const valuesInList = await Object.values(books);
 
-  const booksByAuthor = valuesInList.filter(book => book.author.trim() == author.trim());
+  const booksByAuthor = await valuesInList.filter(book => book.author.trim() == author.trim());
   res.send(booksByAuthor);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   const title = req.params.title;
-  const booksInList = Object.values(books);
+  const booksInList = await Object.values(books);
 
-  const booksByTitle = booksInList.filter(book => book.title.trim() == title.trim());
+  const booksByTitle = await booksInList.filter(book => book.title.trim() == title.trim());
   res.send(booksByTitle);
 });
 
